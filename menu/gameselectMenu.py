f48aed3634752893825893b733a4e72272deeca2
wooyoung0830 <thddndud0830@gmail.com>
2022-12-03 22:02:42 +0900

    FEAT : 메뉴 버튼 클릭시 작동

--- menu/gameselectMenu.py
+++ menu/gameselectMenu.py
@@ -265,51 +265,51 @@ class GameselectMenu:
                 pygame.display.update()
 
 
-            if self.mypage.isOver(pos):
-                if choosed_character == "police":
-                    Mypage_p(self.screen).show()
-                elif choosed_character == "firefighter":
-                    Mypage_f(self.screen).show()
-                elif choosed_character == "doctor":
-                    Mypage_d(self.screen).show()
+                if self.mypage.isOver(pos):
+                    if choosed_character == "police":
+                        Mypage_p(self.screen).show()
+                    elif choosed_character == "firefighter":
+                        Mypage_f(self.screen).show()
+                    elif choosed_character == "doctor":
+                        Mypage_d(self.screen).show()
 
-            if self.rankpage.isOver(pos):
-                LeaderBoardMenu(self.screen).rank()
+                if self.rankpage.isOver(pos):
+                    LeaderBoardMenu(self.screen).rank()
 
-            if self.store.isOver(pos):
-                
-                CharacterStoreMenu(self.screen, choosed_character).show()
+                if self.store.isOver(pos):
+                    
+                    CharacterStoreMenu(self.screen, choosed_character).show()
 
-            if self.help.isOver(pos):
-                HelpMenu(self.screen).show()
+                if self.help.isOver(pos):
+                    HelpMenu(self.screen).show()
 
-            if self.logout.isOver(pos):
-                import Main
-                Main.Login(self.screen).show()
+                if self.logout.isOver(pos):
+                    import Main
+                    Main.Login(self.screen).show()
 
-            if self.returnpage.isOver(pos):  # 직업 선택 화면으로 되돌아가는 페이지
-                from menu.CharacterSelectMenu import CharacterSelect
-                game = CharacterSelect(self.screen)
+                if self.returnpage.isOver(pos):  # 직업 선택 화면으로 되돌아가는 페이지
+                    from menu.CharacterSelectMenu import CharacterSelect
+                    game = CharacterSelect(self.screen)
 
-                while True:
-                    game.show(self.screen)
-                    pygame.display.flip()
+                    while True:
+                        game.show(self.screen)
+                        pygame.display.flip()
 
-            if self.setting.isOver(pos):
-                if self.sound == "on":
-                    self.setting.image = "Image/catthema/off.png"
-                    self.sound = "off"
-                    soundset = 0
-                    print(soundset)
-                    Default.sound.value['sfx']['volume'] = 0
-                    self.character_data = CharacterDataManager.load()  # volume 적용
-                else:
-                    self.setting.image = "Image/catthema/on.png"
-                    self.sound = "on"
-                    soundset = 0.1
-                    print(soundset)
-                    Default.sound.value['sfx']['volume'] = 0.1
-                    self.character_data = CharacterDataManager.load()  # volume 적용
+                if self.setting.isOver(pos):
+                    if self.sound == "on":
+                        self.setting.image = "Image/thema/off.png"
+                        self.sound = "off"
+                        soundset = 0
+                        print(soundset)
+                        Default.sound.value['sfx']['volume'] = 0
+                        self.character_data = CharacterDataManager.load()  # volume 적용
+                    else:
+                        self.setting.image = "Image/thema/on.png"
+                        self.sound = "on"
+                        soundset = 0.1
+                        print(soundset)
+                        Default.sound.value['sfx']['volume'] = 0.1
+                        self.character_data = CharacterDataManager.load()  # volume 적용
 
 
 
